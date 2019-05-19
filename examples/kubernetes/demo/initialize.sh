19047148290229423ad3937c9923005b7ee259ae
Kislay Kishore <kislay.kishore2003@gmail.com>
2019-05-19 20:21:57 +0530

    Include add-ons in the initialization script

--- examples/kubernetes/demo/initialize.sh
+++ examples/kubernetes/demo/initialize.sh
@@ -40,3 +40,18 @@ kubectl get pods --namespace knative-build
 kubectl get pods --namespace knative-eventing
 kubectl get pods --namespace knative-sources
 kubectl get pods --namespace knative-monitoring
+
+# Install Prometheus
+
+curl https://storage.googleapis.com/gke-release/istio/release/1.0.3-gke.3/patches/install-prometheus.yaml | kubectl apply -n istio-system -f -
+
+# Install Grafana
+curl https://storage.googleapis.com/gke-release/istio/release/1.0.3-gke.3/patches/install-grafana.yaml | kubectl apply -n istio-system -f -
+
+# Install Service Graph
+curl https://storage.googleapis.com/gke-release/istio/release/1.0.3-gke.3/patches/install-servicegraph.yaml | kubectl apply -n istio-system -f -
+
+# Install Jaeger (tracing)
+curl https://storage.googleapis.com/gke-release/istio/release/1.0.3-gke.3/patches/install-tracing.yaml | kubectl apply -n istio-system -f -
+
+
--- examples/kubernetes/demo/istio/deployment/curler.sh
+++ examples/kubernetes/demo/istio/deployment/curler.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-addr=`kubectl get svc -n ns5 | sed 1d |awk '{print $4}'`
-port=`kubectl get svc -n ns5 | sed 1d |awk '{print $5}'`
-IFS=':'
-read -ra ADDR <<< "$port"
-port=${ADDR[0]}
-while true
-do
-  curl "http://$addr:$port"
-  printf "\n"
-  sleep 1
-done
+for i in {1..1000}; do curl -w "\n" "http://${GATEWAY_URL}" ; sleep 1; done
